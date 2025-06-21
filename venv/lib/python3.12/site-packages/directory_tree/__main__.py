c1d56fe183ae347a798e5220cbdd196a580ca3fe
WaleedaRaza <133548828+WaleedaRaza@users.noreply.github.com>
2025-06-21 01:50:11 -0400

    Delete venv directory

--- venv/lib/python3.12/site-packages/directory_tree/__main__.py
+++
@@ -1,9 +0,0 @@
-# Importing Necessary Modules
-from directory_tree.tree_cli import directoryTreeCli
-
-# Main entry point for the CLI
-if __name__ == '__main__':
-
-    # Invoking Directory Tree CLI Function
-    directoryTreeCli()
-
